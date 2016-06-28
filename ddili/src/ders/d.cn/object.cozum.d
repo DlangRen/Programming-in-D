4652b9d5d3005e239229cdb1d1e115835a59de60
DFranklike <IlikeDlang@163.com>
2016-06-29 03:34:35 +0800

    翻译完成 'object.cozum.d'

--- ddili/src/ders/d.cn/object.cozum.d
+++ ddili/src/ders/d.cn/object.cozum.d
@@ -1,11 +1,11 @@
 Ddoc
 
-$(COZUM_BOLUMU $(CH4 Object))
+$(COZUM_BOLUMU $(CH4 Object)对象)
 
 $(OL
 
 $(LI
-For the equality comparison, $(C rhs) being non-$(C null) and the members being equal would be sufficient:
+对于相等性比较， $(C rhs) 属非$(C null) 并且成员相等就足够了：
 
 ---
 enum Color { blue, green, red }
@@ -28,7 +28,7 @@ class Point {
 )
 
 $(LI
-When the type of the right-hand side object is also $(C Point), they are compared according to the values of the $(C x) members first and then according to the values of the $(C y) members:
+当右手侧对象的类型也是 $(C Point)，首页根据 $(C x) 成员的值来比较，然后是 $(C y) 成员的值：
 
 ---
 class Point {
@@ -52,7 +52,7 @@ class Point {
 )
 
 $(LI
-Note that it is not possible to cast to type $(C const TriangularArea) inside $(C opCmp) below. When $(C rhs) is $(C const TriangularArea), then its member $(C rhs.points) would be $(C const) as well. Since the parameter of $(C opCmp) is non-$(C const), it would not be possible to pass $(C rhs.points[i]) to $(C point.opCmp).
+注意下面的 $(C opCmp) 函数内部，要转换到 $(C const TriangularArea) 类型是不可能的。当 $(C rhs) 是 $(C const TriangularArea)，那么它的成员 $(C rhs.points) 也将是 $(C const) 。由于 $(C opCmp) 的参数属非$(C const)，它将不可能给 $(C point.opCmp) 传递 $(C rhs.points[i]) 参数。
 
 ---
 class TriangularArea {
@@ -75,21 +75,21 @@ class TriangularArea {
             immutable comparison = point.opCmp(rhs.points[i]);
 
             if (comparison != 0) {
-                /* The sort order has already been
-                 * determined. Simply return the result. */
+                /* 排序顺序已经
+                 * 确定。简单的返回结果。*/
                 return comparison;
             }
         }
 
-        /* The objects are considered equal because all of
-         * their points have been equal. */
+        /* 对象被认为是相等的，因为所有的
+         * point已经相等。*/
         return 0;
     }
 
     override size_t toHash() const {
-        /* Since the 'points' member is an array, we can take
-         * advantage of the existing toHash algorithm for
-         * array types. */
+        /* 由于 'points' 成员是一个数组，我们能
+         * 为数组类型利用现有的 
+          toHash 算法。*/
         return typeid(points).getHash(&points);
     }
 }
@@ -101,8 +101,8 @@ class TriangularArea {
 
 
 Macros:
-        SUBTITLE=Object Solutions
+        SUBTITLE=对象 解答
 
-        DESCRIPTION=Programming in D exercise solutions: Object
+        DESCRIPTION=D语言编程 习题解答：对象
 
-        KEYWORDS=programming in d tutorial Object
+        KEYWORDS=D语言编程教程 Object
