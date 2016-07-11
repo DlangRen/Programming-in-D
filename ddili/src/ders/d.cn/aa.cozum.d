8b9ae273815dad0b330c1062d008d490ce8bf4c0
FrankLIKE <IlikeDlang@163.com>
2016-07-11 12:59:32 +0800

    Update 完成翻译~aa.cozum.d

--- ddili/src/ders/d.cn/aa.cozum.d
+++ ddili/src/ders/d.cn/aa.cozum.d
@@ -1,6 +1,6 @@
 Ddoc
 
-$(COZUM_BOLUMU Associative Arrays)
+$(COZUM_BOLUMU 关联数组)
 
 $(OL
 
@@ -9,7 +9,7 @@ $(LI
 $(UL
 
 $(LI
-The $(C .keys) property returns a slice (i.e. dynamic array) that includes all of the keys of the associative array. Iterating over this slice and removing the element for each key by calling $(C .remove) would result in an empty associative array:
+$(C .keys) 属性返回一个切片(即 动态数组)，它包含了关联数组全部的键。迭代这个切片，通过为每个键调用 $(C .remove) 函数来移除元素，就会形成一个空的关联数组：
 
 ---
 import std.stdio;
@@ -26,8 +26,8 @@ void main() {
 
     int[] keys = names.keys;
 
-    /* 'foreach' is similar but superior to 'for'. We will
-     * see the 'foreach' loop in the next chapter. */
+    /* ‘foreach’相似但优于‘for’。在下一章我们将
+     *看到‘foreach’循环。*/
     foreach (key; keys) {
         writefln("Removing the element %s", key);
         names.remove(key);
@@ -38,13 +38,13 @@ void main() {
 ---
 
 $(P
-That solution may be slow especially for large arrays. The following methods would empty the array in a single step.
+对大数组那样解决会格外慢。下面的方法将用一步清空数组。
 )
 
 )
 
 $(LI
-Another solution is to assign an empty array:
+另一个答案是用空数组赋值：
 
 ---
     string[int] emptyAA;
@@ -54,7 +54,7 @@ Another solution is to assign an empty array:
 )
 
 $(LI
-Since the initial value of an array is an empty array anyway, the following technique would achieve the same result:
+由于数组的初始值无论如何都是一个空数组，那么下面的技术将得到一样的结果：
 
 ---
     names = names.init;
@@ -67,30 +67,30 @@ Since the initial value of an array is an empty array anyway, the following tech
 )
 
 $(LI
-The goal is to store multiple grades per student. Since multiple grades can be stored in a dynamic array, an associative array that maps from $(C string) to $(C int[]) would work here. The grades can be appended to the dynamic arrays that are stored in the associative array:
+目标是存储每个学生的多个成绩。由于多个成绩可以用一个动态数组存储，从 $(C string) 到 $(C int[]) 映射的关联数组将能用在这儿。成绩能附加到存储在关联数组中的动态数组上：
 
 ---
 import std.stdio;
 
 void main() {
-    /* The key type of this associative array is string and
-     * the value type is int[], i.e. an array of ints. The
-     * associative array is being defined with an extra
-     * space in between to help distinguish the value type: */
+    /* 关联数组的键是字符串，
+     *值的类型是 int[]，即整型数组。The
+     *关联数组将定义额外的空间，
+     * 来帮助区分值类型： */
     int[] [string] grades;
 
-    /* The array of ints that correspond to "emre" is being
-     * used for appending the new grade to that array: */
+    /* 与“emre” 对应的整型数组
+     * 将用于附加新成绩：*/
     grades["emre"] ~= 90;
     grades["emre"] ~= 85;
 
-    /* Printing the grades of "emre": */
+    /* 打印“emre”的成绩： */
     writeln(grades["emre"]);
 }
 ---
 
 $(P
-The grades can also be assigned in one go with an array literal:
+成绩也可以作为数组文本一次性赋值：
 )
 
 ---
@@ -110,8 +110,8 @@ void main() {
 )
 
 Macros:
-        SUBTITLE=Associative Arrays Solutions
+        SUBTITLE=关联数组 习题解答
 
-        DESCRIPTION=Programming in D exercise solutions: Associative Arrays
+        DESCRIPTION=D语言习题解答：关联数组
 
-        KEYWORDS=programming in d tutorial associative arrays
+        KEYWORDS=D语言教程 关联数组
