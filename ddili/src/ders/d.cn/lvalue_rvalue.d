a3aeea03f15b641883494b69b32714d04e7c1ffe
mogud <mogudx@163.com>
2016-07-22 17:29:35 +0800

    Update 翻译完成 ~ lvalue_rvalue.d

--- ddili/src/ders/d.cn/lvalue_rvalue.d
+++ ddili/src/ders/d.cn/lvalue_rvalue.d
@@ -1,13 +1,13 @@
 Ddoc
 
-$(DERS_BOLUMU $(IX lvalue) $(IX rvalue) Lvalues and Rvalues)
+$(DERS_BOLUMU $(IX 左值) $(IX 右值) 左值与右值)
 
 $(P
-$(IX expression, lvalue vs rvalue) The value of every expression is classified as either an lvalue or an rvalue. A simple way of differentiating the two is thinking of lvalues as actual variables (including elements of arrays and associative arrays), and rvalues as temporary results of expressions (including literals).
+$(IX 表达式, 左值与右值) 任何表达式的值都可以用左值或右值归类。要区分他们可以简单地认为左值是真实变量（包括数组和关联数组的元素），而右值是表达式（包括文字量）的临时结果。
 )
 
 $(P
-As a demonstration, the first $(C writeln()) expression below uses only lvalues and the other one uses only rvalues:
+例如，下面的第一个 $(C writeln()) 表达式只使用了左值，其他的只使用了右值：
 )
 
 ---
@@ -19,24 +19,24 @@ void main() {
     auto arr = [ 1 ];
     auto aa = [ 10 : "ten" ];
 
-    /* All of the following arguments are lvalues. */
+    /* 下面所有参数都是左值。 */
 
-    writeln(i,          // mutable variable
-            imm,        // immutable variable
-            arr,        // array
-            arr[0],     // array element
-            aa[10]);    // associative array element
-                        // etc.
+    writeln(i,          // 可变变量
+            imm,        // 不可变变量
+            arr,        // 数组
+            arr[0],     // 数组元素
+            aa[10]);    // 关联数组元素
+                        // 等
 
     enum message = "hello";
 
-    /* All of the following arguments are rvalues. */
+    /* 下面所有参数都是右值。 */
 
-    writeln(42,               // a literal
-            message,          // a manifest constant
-            i + 1,            // a temporary value
-            calculate(i));    // return value of a function
-                              // etc.
+    writeln(42,               // 一个文字量
+            message,          // 显然是一个常量
+            i + 1,            // 一个临时值
+            calculate(i));    // 函数的返回值
+                              // 等
 }
 
 int calculate(int i) {
@@ -44,20 +44,20 @@ int calculate(int i) {
 }
 ---
 
-$(H5 Limitations of rvalues)
+$(H5 右值的限制)
 
 $(P
-Compared to lvalues, rvalues have the following three limitations.
+相对于左值，右值有下面三条限制。
 )
 
-$(H6 Rvalues don't have memory addresses)
+$(H6 右值没有存储地址)
 
 $(P
-An lvalue has a memory location to which we can refer, while an rvalue does not.
+左值有可以引用的存储地址，但右值没有。
 )
 
 $(P
-For example, it is not possible to take the address of the rvalue expression $(C a&nbsp;+&nbsp;b) in the following program:
+例如，下面的程序里不可能取到右值表达式 $(C a&nbsp;+&nbsp;b) 的值：
 )
 
 ---
@@ -67,7 +67,7 @@ void main() {
     int a;
     int b;
 
-    readf(" %s", &a);          $(CODE_NOTE compiles)
+    readf(" %s", &a);          $(CODE_NOTE 编译通过)
     readf(" %s", &(a + b));    $(DERLEME_HATASI)
 }
 ---
@@ -76,14 +76,14 @@ $(SHELL
 Error: a + b $(HILITE is not an lvalue)
 )
 
-$(H6 Rvalues cannot be assigned new values)
+$(H6 右值不能被赋新值)
 
 $(P
-If mutable, an lvalue can be assigned a new value, while an rvalue cannot be:
+左值如果为可变，则可以被赋予新值，但右值确不行：
 )
 
 ---
-    a = 1;          $(CODE_NOTE compiles)
+    a = 1;          $(CODE_NOTE 编译通过)
     (a + b) = 2;    $(DERLEME_HATASI)
 ---
 
@@ -91,10 +91,10 @@ $(SHELL
 Error: a + b $(HILITE is not an lvalue)
 )
 
-$(H6 Rvalues cannot be passed to functions by reference)
+$(H6 右值不能通过引用方式传递给函数)
 
 $(P
-An lvalue can be passed to a function that takes a parameter by reference, while an rvalue cannot be:
+左值能传递给一个接受引用参数的函数，但右值不能：
 )
 
 ---
@@ -104,7 +104,7 @@ void incrementByTen($(HILITE ref int) value) {
 
 // ...
 
-    incrementByTen(a);        $(CODE_NOTE compiles)
+    incrementByTen(a);        $(CODE_NOTE 编译通过)
     incrementByTen(a + b);    $(DERLEME_HATASI)
 ---
 
@@ -114,11 +114,11 @@ $(HILITE is not callable) using argument types (int)
 )
 
 $(P
-The main reason for this limitation is the fact that a function taking a $(C ref) parameter can hold on to that reference for later use, at a time when the rvalue would not be available.
+有这个限制的主要原因是接受一个引用参数的函数可以保持这个引用在稍后使用，而在那个时间点右值却已经失效了。
 )
 
 $(P
-Different from languages like C++, in D an rvalue cannot be passed to a function even if that function does $(I not) modify the argument:
+与如 C++ 等语言不同，在 D 中就算函数$(I 不)改变实参，右值也不能传递给函数：
 )
 
 ---
@@ -128,7 +128,7 @@ void print($(HILITE ref const(int)) value) {
 
 // ...
 
-    print(a);        $(CODE_NOTE compiles)
+    print(a);        $(CODE_NOTE 编译通过)
     print(a + b);    $(DERLEME_HATASI)
 ---
 
@@ -137,25 +137,24 @@ Error: function deneme.print (ref const(int) value)
 $(HILITE is not callable) using argument types (int)
 )
 
-$(H5 $(IX auto ref, parameter) $(IX parameter, auto ref) Using $(C auto ref) parameters to accept both lvalues and rvalues)
+$(H5 $(IX auto ref, 参数) $(IX 参数, auto ref) 使用 $(C auto ref) 参数可以同时接受左值和右值)
 
 $(P
-As was mentioned in the previous chapter, $(C auto ref) parameters of $(LINK2 /ders/d.en/templates.html, function templates) can take both lvalues and rvalues.
+如前面章节中提到的，$(LINK2 /ders/d.cn/templates.html, 函数模板)的 $(C auto ref) 参数可以同时接受左值和右值。
 )
 
 $(P
-When the argument is an lvalue, $(C auto ref) means $(I by reference). On the other hand, since rvalues cannot be passed to functions by reference, when the argument is an rvalue, it means $(I by copy). For the compiler to generate code differently for these two distinct cases, the function must be a template.
+当参数是一个左值时，$(C auto ref) 表示$(I 传引用)。另一方面，因为右值不能通过引用传给函数，所以当参数是一个右值时，它表示是$(I 传值)。由于编译器要为两种不同的情况生成不同的代码，因此这个函数必须是一个模板。
 )
 
 $(P
-We will see templates in a later chapter. For now, please accept that the highlighted empty parentheses below make the following definition a $(I function template).
+我们会在后面的章节中看到模板。现在，请先了解下面高亮的括号让这个定义变成了一个$(I 函数)模板。
 )
 
 ---
 void incrementByTen$(HILITE ())($(HILITE auto ref) int value) {
-    /* WARNING: The parameter may be a copy if the argument is
-     * an rvalue. This means that the following modification
-     * may not be observable by the caller. */
+    /* 警告：如果实参是一个右值则形参会是一个拷贝。也就是说下面的更改对
+     * 对调用者来说不可见。 */
 
     value += 10;
 }
@@ -164,41 +163,41 @@ void main() {
     int a;
     int b;
 
-    incrementByTen(a);        $(CODE_NOTE lvalue; passed by reference)
-    incrementByTen(a + b);    $(CODE_NOTE rvalue; copied)
+    incrementByTen(a);        $(CODE_NOTE 左值; 引用传递)
+    incrementByTen(a + b);    $(CODE_NOTE 右值; 拷贝)
 }
 ---
 
 $(P
-As mentioned in the code comment above, the mutation to the parameter may not be observable by the caller. For that reason, $(C auto ref) is mostly used in situations where the parameter is not modified; it is used mostly as $(C auto ref const).
+正如上面代码注释提到的，对形参的改动可能会对调用者不可见。因此，$(C auto ref) 最常用于形参不会被改变的场景；通常写作 $(C auto ref const)。
 )
 
-$(H5 Terminology)
+$(H5 术语)
 
 $(P
-The names "lvalue" and "rvalue" do not represent the characteristics of these two kinds of values accurately. The initial letters $(I l) and $(I r) come from $(I left) and $(I right), referring to the left- and the right-hand side expressions of the assignment operator:
+“左值（lvalue）”和“右值（rvalue）”这两个名字并不能准确地表示两种值的特性。第一个字母 $(I l) 和 $(I r) 源自 $(I 左（left）) 和 $(I 右（right）)，表示赋值运算符左边或右边表达式：
 )
 
 $(UL
 
-$(LI Assuming that it is mutable, an lvalue can be the left-hand expression of an assignment operation.)
+$(LI 左值如果可变，则可以是赋值运算左边的表达式。)
 
-$(LI An rvalue cannot be the left-hand expression of an assignment operation.)
+$(LI 右值不能是赋值运算左边的表达式。)
 
 )
 
 $(P
-The terms "left value" and "right value" are confusing because in general both lvalues and rvalues can be on either side of an assignment operation:
+术语“左值”和“右值”会有些混淆，因为通常左值和右值都可以出现在赋值运算的任何一边：
 )
 
 ---
-    // rvalue 'a + b' on the left, lvalue 'a' on the right:
+    // 右值“a + b”在左边，左值“a”在右边：
     array[a + b] = a;
 ---
 
 Macros:
-        SUBTITLE=Lvalues and Rvalues
+        SUBTITLE=左值与右值
 
-        DESCRIPTION=Left-values and right-values and their differences.
+        DESCRIPTION=左值与右值及它们的区别。
 
-        KEYWORDS=d programming language tutorial book lvalue rvalue
+        KEYWORDS=d 编程 语言 教程 左值 右值
