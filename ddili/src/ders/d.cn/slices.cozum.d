94d102780a567fb8ae044b17d1601eea0947f498
FrankLIKE <IlikeDlang@163.com>
2016-09-06 07:53:30 +0800

    Update ~slices.d,slices.cozum.d

--- ddili/src/ders/d.cn/slices.cozum.d
+++ ddili/src/ders/d.cn/slices.cozum.d
@@ -3,7 +3,7 @@ Ddoc
 $(COZUM_BOLUMU Slices and Other Array Features)
 
 $(P
-Iterating over elements by consuming a slice from the beginning is an interesting concept. This method is also the basis of Phobos ranges that we will see in a later chapter.
+通过从开始处缩减切片来遍历元素是一个有意思的想法。在后面的一章中我们将看到这个方法也是 Phobos 的 range 的基本方法。
 )
 
 ---
@@ -12,29 +12,29 @@ import std.stdio;
 void main() {
     double[] array = [ 1, 20, 2, 30, 7, 11 ];
 
-    double[] slice = array;    // Start with a slice that
-                               // provides access to all of
-                               // the elements of the array
+    double[] slice = array;    // 允许切片
+                               // 访问数组的
+                               // 所有元素
 
-    while (slice.length) {     // As long as there is at least
-                               // one element in that slice
+    while (slice.length) {     // 只要切片
+                               // 至少有一个元素
 
-        if (slice[0] > 10) {   // Always use the first element
-            slice[0] /= 2;     // in the expressions
+        if (slice[0] > 10) {   // 在表达式中
+            slice[0] /= 2;     // 总是使用第一个元素
         }
 
-        slice = slice[1 .. $]; // Shorten the slice from the
-                               // beginning
+        slice = slice[1 .. $]; // 从开始处缩短
+                               // 切片
     }
 
-    writeln(array);            // The actual elements are
-                               // changed
+    writeln(array);            // 实际元素已经
+                               // 发生变化
 }
 ---
 
 Macros:
-        SUBTITLE=Slices and Other Array Features Solution
+        SUBTITLE=切片和其它的数组特征习题解答
 
-        DESCRIPTION=Programming in D exercise solutions: arrays
+        DESCRIPTION=D 语言编程习题解答：数组
 
-        KEYWORDS=programming in d tutorial arrays solution
+        KEYWORDS=D 语言编程教程 数组 习题解答
