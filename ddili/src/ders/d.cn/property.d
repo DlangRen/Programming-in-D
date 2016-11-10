8009153dfa632b5a15807a4c36cb5ba7434d34c3
IceNature <icenaturedzc47@qq.com>
2016-11-10 18:27:55 +0800

    Update 完成翻译 ~ property.d

--- ddili/src/ders/d.cn/property.d
+++ ddili/src/ders/d.cn/property.d
@@ -1,13 +1,13 @@
 Ddoc
 
-$(DERS_BOLUMU $(IX property) Properties)
+$(DERS_BOLUMU $(IX property) 属性)
 
 $(P
-Properties allow using member functions like member variables.
+属性能让我们使用像访问成员变量一样的语法调用成员函数。
 )
 
 $(P
-We are familiar with this feature from slices. The $(C length) property of a slice returns the number of elements of that slice:
+在学习分片的时候我们已经多次用到它。slice 的 $(C length) 属性返回了其包含元素的个数：
 )
 
 ---
@@ -16,7 +16,7 @@ We are familiar with this feature from slices. The $(C length) property of a sli
 ---
 
 $(P
-Looking merely at that usage, one might think that $(C .length) has been implemented as a member variable:
+如果只看它的语法，你可能会认为 $(C .length) 是一个成员变量：
 )
 
 ---
@@ -28,48 +28,48 @@ struct SliceImplementation {
 ---
 
 $(P
-However, the other functionality of this property proves that it cannot be a member variable: Assigning a new value to the $(C .length) property actually changes the length of the slice, sometimes by adding new elements to the underlying array:
+但属性提供了成员变量所不能提供的功能：对 $(C .length) 赋值将会改变 slice 的真实长度。向数组中添加新元素时就包含这一过程：
 )
 
 ---
-    slice$(HILITE .length = 5);    // The slice now has 5 elements
+    slice$(HILITE .length = 5);    // slice 现在有 5 个元素
     assert(slice.length == 5);
 ---
 
-$(P $(I $(B Note:) The $(C .length) property of fixed-length arrays cannot be modified.)
+$(P $(I $(B 注：)无法修改定长数组的 $(C .length) 属性。)
 )
 
 $(P
-The assignment to $(C .length) above involves more complicated operations than a simple value change: Determining whether the array has capacity for the new length, allocating more memory if not, and moving the existing elements to the new place; and finally initializing each additional element by $(C .init).
+对 $(C .length) 赋值并不是简单的改变某个变量，实际上语言在后台做了很多复杂的工作：判断数组是否有足够的空间容纳新的元素，如果没有则在内存中分配更大的空间并将已存在的元素移动到新位置；最后再将添加的新元素以 $(C .init) 的方式初始化。
 )
 
 $(P
-Evidently, the assignment to $(C .length) operates like a function.
+就像你看到的那样，对 $(C .length) 的赋值就像是调用了一个函数。
 )
 
 $(P
-$(IX @property) Properties are member functions that are used like member variables. They are defined by the $(C @property) attribute.
+$(IX @property) 属性实际上就是成员函数，但我们却可以像使用成员变量一样调用它们。所需的额外工作就是在函数的前面加一个 $(C @property)。
 )
 
-$(H5 Calling functions without parentheses)
+$(H5 不使用圆括号的函数调用)
 
 $(P
-$(IX ()) As has been mentioned in the previous chapter, when there is no argument to pass, functions can be called without parentheses:
+$(IX ()) 我们在之前的章节中提到过，如果调用函数时不需要传递参数，那我们可以省略圆括号：
 )
 
 ---
     writeln();
-    writeln;      // Same as the previous line
+    writeln;      // 与上一行相同
 ---
 
 $(P
-This feature is closely related to properties because properties are used almost always without parentheses.
+这个特性和属性很像，它们都不需要圆括号。
 )
 
-$(H5 Property functions that return values)
+$(H5 读属性)
 
 $(P
-As a simple example, let's consider a rectangle struct that consists of two members:
+我们使用下面这个包含两个成员的矩形结构体作为示例：
 )
 
 ---
@@ -80,7 +80,7 @@ struct Rectangle {
 ---
 
 $(P
-Let's assume that a third property of this type becomes a requirement, which should provide the area of the rectangle:
+现在假设我们需要为这个类型添加一个新属性用于表示这个矩形的面积：
 )
 
 ---
@@ -89,7 +89,7 @@ Let's assume that a third property of this type becomes a requirement, which sho
 ---
 
 $(P
-One way of achieving that requirement is to define a third member:
+一种方案是在矩形结构体中定义第三个成员变量：
 )
 
 ---
@@ -101,20 +101,20 @@ struct Rectangle {
 ---
 
 $(P
-A flaw in that design is that the object may easily become inconsistent: Although rectangles must always have the invariant of "width * height == area", this consistency may be broken if the members are allowed to be modified freely and independently.
+这种设计的缺点是对象很容易出现不一致性：虽然矩形永远都是“长 × 宽 == 面积”，但是如果成员变量能被随意修改的话，对象的一致性将极易被破坏。
 )
 
 $(P
-As an extreme example, objects may even begin their lives in inconsistent states:
+作为一个极端的例子，对象在其生命期开始时就已经处在不一致的状态了：
 )
 
 ---
-    // Inconsistent object: The area is not 10 * 20 == 200.
+    // 不一致的对象：面积不是 10 * 20 == 200。
     auto garden = Rectangle(10, 20, $(HILITE 1111));
 ---
 
 $(P
-A better way would be to represent the concept of area as a property. Instead of defining an additional member, the value of that member is calculated by a function named $(C area), the same as the concept that it represents:
+所以我们最好用属性来实现面积。我们要定义一个名为 $(C area) 的函数来计算面积而不是定义成员变量来表示面积：
 )
 
 ---
@@ -128,11 +128,11 @@ struct Rectangle {
 }
 ---
 
-$(P $(I $(B Note:) As you would remember from $(LINK2 /ders/d.en/const_member_functions.html, the $(CH4 const ref) Parameters and $(CH4 const) Member Functions chapter), the $(C const) specifier on the function declaration ensures that the object is not modified by this function.)
+$(P $(I $(B 注：)如果你还记得我们在 $(LINK2 /ders/d.cn/const_member_functions.html, $(CH4 const ref) 参数和 $(CH4 const) 成员函数) 一章中所讲的，函数声明中的 $(C const) 说明符表示函数不会修改其对象。)
 )
 
 $(P
-That property function enables the struct to be used as if it has a third member variable:
+属性函数让我们能像使用成员变量一样使用它：
 )
 
 ---
@@ -141,17 +141,17 @@ That property function enables the struct to be used as if it has a third member
 ---
 
 $(P
-As the value of the $(C area) property is calculated by multiplying the width and the height of the rectangle, this time it would always be consistent:
+$(C area) 是由矩形的长宽相乘计算得到的，所以这次它将始终与长宽保持一致：
 )
 
 $(SHELL
 The area of the garden: 200
 )
 
-$(H5 Property functions that are used in assignment)
+$(H5 写属性)
 
 $(P
-Similar to the $(C length) property of slices, the properties of user-defined types can be used in assignment operations as well:
+和 slices 的 $(C length) 属性一样，用户定义的属性也可以用于赋值：
 )
 
 ---
@@ -159,15 +159,15 @@ Similar to the $(C length) property of slices, the properties of user-defined ty
 ---
 
 $(P
-For that assignment to actually change the area of the rectangle, the two members of the struct must be modified accordingly. To enable this functionality, we can assume that the rectangle is $(I flexible) so that to maintain the invariant of "width * height == area", the sides of the rectangle can be changed.
+改变矩形面积时也要将长和宽修改为对应的值。为了能够提供这样的功能，我们假设矩形的边长是$(I 可变的)。这样这个矩形就保持恒等式“长 × 宽 == 面积”成立。
 )
 
 $(P
-The function that enables such an assignment syntax is also named as $(C area) and is also marked by $(C @property). The value that is used on the right-hand side of the assignment becomes the only parameter of this function.
+只需你将函数命名为 $(C area) 并将其标记为 $(C @property)，你就能够使用赋值语法调用它。等号右侧的值将作为函数唯一的参数传入函数。
 )
 
 $(P
-The following additional definition of $(C area()) enables using that property in assignment operations and effectively modifying the area of $(C Rectangle) objects:
+下面新增的 $(C area()) 让我们可以对 $(C area) 使用赋值运算符，而且对它赋值的确会改变 $(C Rectangle) 对象的面积：
 )
 
 ---
@@ -202,11 +202,11 @@ void main() {
 ---
 
 $(P
-The new function takes advantage of the $(C sqrt) function from the $(C std.math) module, which returns the square root of the specified value. When both of the width and the height of the rectangle are scaled by the square root of the ratio, then the area would equal the desired value.
+新增的属性函数利用 $(C std.math) 模块中的 $(C sqrt) 函数来开平方根。如果长和宽都是面积的平方根，那它们的乘积就一定是我们指定的面积。
 )
 
 $(P
-As a result, assigning the quarter of its current value to $(C area) ends up halving both sides of the rectangle:
+对这个版本的 $(C area) 赋值时矩形两边的长度都会改变：
 )
 
 $(SHELL
@@ -214,10 +214,10 @@ The area of the garden: 200
 New state: 5 x 10 = 50
 )
 
-$(H5 Properties are not absolutely necessary)
+$(H5 属性不是唯一的解决方案)
 
 $(P
-We have seen above how $(C Rectangle) can be used as if it has a third member variable. However, regular member functions could also be used instead of properties:
+之前我们通过定义属性为 $(C Rectangle) 添加了“第三个变量”。当然，除了属性普通，成员函数也可以实现我们需要的功能：
 )
 
 ---
@@ -252,7 +252,7 @@ void main() {
 ---
 
 $(P
-Further, as we have seen in $(LINK2 /ders/d.en/function_overloading.html, the Function Overloading chapter), these two functions could even have the same names:
+就像我们在 $(LINK2 /ders/d.cn/function_overloading.html, 函数重载) 一章中提到过的，这两个函数可以有相同的名字。
 )
 
 ---
@@ -265,18 +265,18 @@ Further, as we have seen in $(LINK2 /ders/d.en/function_overloading.html, the Fu
     }
 ---
 
-$(H5 When to use)
+$(H5 何时使用属性)
 
 $(P
-It may not be easy to chose between regular member functions and properties. Sometimes regular member functions feel more natural and sometimes properties.
+并没有一种简单的方法能帮你快速在常规成员函数和属性间做出选择。有时常规成员函数更加自然，有时属性更加简洁。
 )
 
 $(P
-However, as we have seen in $(LINK2 /ders/d.en/encapsulation.html, the Encapsulation and Protection Attributes chapter), it is important to restrict direct access to member variables. Allowing user code to freely modify member variables always ends up causing issues with code maintenance. For that reason, member variables better be encapsulated either by regular member functions or by property functions.
+我们曾在 $(LINK2 /ders/d.cn/encapsulation.html, 封装和访问控制) 一章中告诉过你：限制外部代码对成员变量的访问是非常重要的。允许外部代码随意修改成员变量可能会对将来的维护工作造成极大的不便。所以，我们最好将对成员变量的访问封装在常规成员函数或属性函数中。
 )
 
 $(P
-Leaving members like $(C width) and $(C height) open to $(C public) access is acceptable only for very simple types. Almost always a better design is to use property functions:
+像 $(C width) 和 $(C height) 简单类型的成员变量，$(C public) 访问权限也是可以接受的。但最好还是使用属性函数包装：
 )
 
 ---
@@ -310,15 +310,15 @@ public:
 ---
 
 $(P
-Note how the members are made $(C private) so that they can only be accessed by corresponding property functions.
+现在成员变量被封装为 $(C private)，所以只有与之相关联的属性函数可以访问它。
 )
 
 $(P
-Also note that to avoid confusing their names with the member functions, the names of the member variables are appended by the $(C _) character. $(I Decorating) the names of member variables is a common practice in object oriented programming.
+注意不要混淆了变量和函数的名字：成员变量的名字多了一个 $(C _)。在面向对象编程中，$(I 修饰)变量名是十分常用的技巧。
 )
 
 $(P
-That definition of $(C Rectangle) still presents $(C width) and $(C height) as if they are member variable:
+$(C Rectangle) 的定义依旧存在 $(C width) 和 $(C height)，它们也依旧表现的像是成员变量。
 )
 
 ---
@@ -328,7 +328,7 @@ That definition of $(C Rectangle) still presents $(C width) and $(C height) as i
 ---
 
 $(P
-When there is no property function that modifies a member variable, then that member is effectively read-only from the outside:
+如果成员变量没有写属性，那在对象外部看来它将是只读的：
 )
 
 ---
@@ -336,16 +336,16 @@ When there is no property function that modifies a member variable, then that me
 ---
 
 $(P
-This is important for controlled modifications of members. The member variables can only be modified by the $(C Rectangle) type itself to ensure the consistency of its objects.
+对成员变量的写入控制也是非常重要的。只有 $(C Rectangle) 的成员函数能修改成员变量可以保证对象的一致性。
 )
 
 $(P
-When it later makes sense that a member variable should be allowed to be modified from the outside, then it is simply a matter of defining another property function for that member.
+如果将来的某一天这个成员变量需要在外部修改，那我们仅需为其添加一个属性函数。
 )
 
 Macros:
-        SUBTITLE=Properties
+        SUBTITLE=属性
 
-        DESCRIPTION=Enabling using member functions like member variables.
+        DESCRIPTION=像访问成员变量一样调用成员函数
 
-        KEYWORDS=d programming lesson book tutorial property
+        KEYWORDS=D 编程语言教程 属性
