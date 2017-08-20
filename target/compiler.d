04f648715093ba05775404d70b2bc785dae54fe4
BitWorld <bitworld@qq.com>
2017-08-20 08:54:28 +0800

    Project translation

--- target/compiler.d
+++ target/compiler.d
@@ -1,32 +1,32 @@
 Ddoc
 
-$(DERS_BOLUMU $(IX compilation) Compilation)
+$(DERS_BOLUMU $(IX compilation) 编译)
 
 $(P
-We have seen that the two tools that are used most in D programming are $(I the text editor) and $(I the compiler). D programs are written in text editors.
+用 D 编程时经常会用到两个工具，它们分别是 $(I 文本编辑器) 和 $(I 编译器)。D 程序需要在文本编辑器里编写。
 )
 
 $(P
-The concept of compilation and the function of the compiler must also be understood when using $(I compiled) languages like D.
+当使用类似 D 语言的 $(I 编译型) 语言时，需要理解编译的概念和编译器的功能。
 )
 
-$(H5 $(IX machine code) Machine code)
+$(H5 $(IX machine code) 机器码)
 
 $(P
-$(IX CPU) $(IX microprocessor) The brain of the computer is the microprocessor (or the CPU, short for $(I central processing unit)). Telling the CPU what to do is called $(I coding), and the instructions that are used when doing so are called $(I machine code).
-)
-
-$(P
-Most CPU architectures use machine code specific to that particular architecture. These machine code instructions are determined under hardware constraints during the design stage of the architecture. At the lowest level these machine code instructions are implemented as electrical signals. Because the ease of coding is not a primary consideration at this level, writing programs directly in the form of the machine code of the CPU is a very difficult task.
+$(IX CPU) $(IX microprocessor) 计算机的大脑是微处理器（CPU，即 $(I 中央处理单元) 的简称)。告知 CPU 要做什么事情这在过程叫做 $(I 编码)，而何时该做何事的指令则叫做 $(I 机器码)。
 )
 
 $(P
-These machine code instructions are special numbers, which represent various operations supported by the CPU. For example, for an imaginary 8-bit CPU, the number 4 might represent the operation of loading, the number 5 might represent the operation of storing, and the number 6 might represent the operation of incrementing. Assuming that the leftmost 3 bits are the operation number and the rightmost 5 bits are the value that is used in that operation, a sample program in machine code for this CPU might look like the following:
+大多数 CPU 都使用特定架构的机器码。而这些机器码指令取决于底层架构设计阶段时的硬件约束。它们在最底层由电子信号实现。在这层，编码的舒适度不是主要考虑因素，所以直接按照 CPU 机器码形式编写程序非常困难。
+)
+
+$(P
+机器码指令都是特定的数字，它们表示 CPU 支持的各种操作。例如，对于虚构的 8 位 CPU 来说，数字 4 可能表示载入操作，数字 5 可能表示存储操作，而数字 6 可能表示递增操作。假定最左边 3 位表示操作码，最右边 5 位表示用于运算的值，该 8 位 CPU 上机器码示例程序如下所示：
 )
 
 $(MONO
 $(B
-Operation   Value            Meaning)
+操作   值            含义)
    100      11110        LOAD      11110
    101      10100        STORE     10100
    110      10100        INCREMENT 10100
@@ -34,13 +34,13 @@ Operation   Value            Meaning)
 )
 
 $(P
-Being so close to hardware, machine code is not suitable for representing higher level concepts like $(I a playing card) or $(I a student record).
+由于机器码太靠近硬件，因此它并不适合用于表示 $(I 扑克牌) 或 $(I 学生记录) 等较为高级的概念。
 )
 
-$(H5 $(IX programming language) Programming language)
+$(H5 $(IX programming language) 编程语言)
 
 $(P
-Programming languages are designed as efficient ways of programming a CPU, capable of representing higher-level concepts. Programming languages do not have to deal with hardware constraints; their main purposes are ease of use and expressiveness. Programming languages are easier for humans to understand, closer to natural languages:
+编程语言的设计目标是用来对 CPU 高效编程，且有能力描述高级概念。编程语言不处理底层硬件约束，它们的主要目的是使用和表现上的简易舒适。编程语言更易于人类理解，更接近自然语言：
 )
 
 $(MONO
@@ -50,52 +50,52 @@ if (a_card_has_been_played()) {
 )
 
 $(P
-However, programming languages adhere to much more strict and formal rules than any spoken language.
+然而，编程语言依然比口语更严格、正规。
 )
 
-$(H5 $(IX interpreter) Interpreter)
+$(H5 $(IX interpreter) 解释器)
 
 $(P
-An interpreter is a tool (a program) that reads the instructions from source code and executes them. For example, for the code above, an interpreter would understand to first execute $(C a_card_has_been_played()) and then conditionally execute $(C display_the_card()). From the point of view of the programmer, executing with an interpreter involves just two steps: writing the source code and giving it to the interpreter.
-)
-
-$(P
-The interpreter must read and understand the instructions every time the program is executed. For that reason, running a program with an interpreter is usually slower than running the compiled version of the same program. Additionally, interpreters usually perform very little analysis on the code before executing it. As a result, most interpreters discover programming errors only after they start executing the program.
+解释器是一种能从源码中读取指令并执行相应命令的工具（程序）。例如，对于上述代码，解释器会首先执行 $(C a_card_has_been_played()) ，而后根据其执行结果选择性地执行 $(C display_the_card())。从程序员的角度来看，依赖解释器执行指令只涉及两个步骤：编写源代码和将源代码交由解释器处理。
 )
 
 $(P
-Some languages like Perl, Python and Ruby have been designed to be very flexible and dynamic, making code analysis harder. These languages have traditionally been used with an interpreter.
-)
-
-$(H5 $(IX compiler) Compiler)
-
-$(P
-A compiler is another tool that reads the instructions of a program from source code. Different from an interpreter, it does not execute the code; rather, it produces a program written in another language (usually machine code). This produced program is responsible for the execution of the instructions that were written by the programmer. From the point of view of the programmer, executing with a compiler involves three steps: writing the source code, compiling it, and running the produced program.
+每一次执行程序之前，解释器都必须重新读入源码并了解它们的含义。这也就是为什么使用解释器执行程序往往要比直接执行编译版本的同一程序要慢。此外，解释器在执行代码之前对代码的分析大都非常有限。最后，大多数解释器在执行程序之后方能发现编程错误。
 )
 
 $(P
-Unlike an interpreter, the compiler reads and understands the source code only once, during compilation. For that reason and in general, a compiled program runs faster compared to executing that program with an interpreter. Compilers usually perform advanced analysis on the code, which help with producing fast programs and catching programming errors before the program even starts running. On the other hand, having to compile the program every time it is changed is a complication and a potential source of human errors. Moreover, the programs that are produced by a compiler can usually run only on a specific platform; to run on a different kind of processor or on a different operating system, the program would have to be recompiled. Additionally, the languages that are easy to compile are usually less dynamic than those that run in an interpreter.
+一些语言被设计得非常灵活与动态，诸如 Perl、Python 和 Ruby，这使得代码分析困难重重。因此，这些语言传统上就需使用解释器。
+)
+
+$(H5 $(IX compiler) 编译器)
+
+$(P
+编译器是另一种从源码中读取程序指令的工具。与解释器不同的是，它不立即执行代码，而是生成一个用其他语言（通常为机器码）表示的程序。而后，这个被生成的程序负责执行程序员写就的指令。这时，从程序员的角度来看，执行指令就分三个步骤：写代码、编译和执行生成的文件。
 )
 
 $(P
-For reasons like safety and performance, some languages have been designed to be compiled. Ada, C, C++, and D are some of them.
-)
-
-$(H6 $(IX error, compilation) $(IX compilation error) Compilation error)
-
-$(P
-As the compiler compiles a program according to the rules of the language, it stops the compilation as soon as it comes across $(I illegal) instructions. Illegal instructions are the ones that are outside the specifications of the language. Problems like a mismatched parenthesis, a missing semicolon, a misspelled keyword, etc. all cause compilation errors.
+不像解释器，编译器一次性地在编译期读入和理解源码。出于这个原因，通常情况下被编译的程序较被解释器所执行的程序运行更快。编译器素来对源码进行深入的分析，这有助于生成更高效的程序，甚至于在程序执行之前就捕捉到编程错误。另一方面，每次改变代码都必须重新编译，既复杂又易引起人为错误。另外，编译器往往是针对某一特定平台进行编译，程序需要重新编译以在不同的处理器或操作系统上运行。此外，容易编译的语言与在解释器中运行的语言相比往往缺乏灵活性。
 )
 
 $(P
-The compiler may also emit a $(I compilation warning) when it sees a suspicious piece of code that may cause concern but not necessarily an error. However, warnings almost always indicate an actual error or bad style, so it is a common practice to consider most or all warnings as errors. The $(C dmd) compiler switch to enable warnings as errors is $(C -w).
+出于对安全及效率等原因的考量，某些语言被设计为可编译的。Ada、C、C++ 和 D 都属于这样的语言。
+)
+
+$(H6 $(IX error, compilation) $(IX compilation error) 编译错误)
+
+$(P
+当编译器按照语言规则编译程序时，一旦碰到 $(I 非法) 指令就会停止编译。非法指令指的是超出语言规范的地方。诸如圆括号未匹配、分号丢失、关键字拼错等的问题都会引起编译错误。
+)
+
+$(P
+当编译器看到某段可疑但又不是错误的代码时，会发出 $(I 编译警告)。但是，警告几乎总是指示一个实际错误或糟糕的代码风格，所以把警告当作错误对待是一个常见的做法。编译器 $(C dmd) 将警告视作错误的编译选项是 $(C -w)。
 )
 
 $(Ergin)
 
 Macros:
-        SUBTITLE=Compiler
+        SUBTITLE=编译器
 
-        DESCRIPTION=The introduction of the compiler and compiled languages
+        DESCRIPTION=编译与编译型编程语言简介
 
-        KEYWORDS=d programming language tutorial book
+        KEYWORDS=D 编程 语言 教程 书籍
